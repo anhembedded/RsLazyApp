68ee092cc4f936162aabf02cfd392ae2f2f5911f
AnhTH49 <anhembedded@gmail.com>
2025-02-23 00:26:54 +0700

    change naming convention

--- views/view_abstract.py
+++ views/view_abstract.py
@@ -1,14 +1,14 @@
 from abc import abstractmethod
 from utility.log import Logger_T, logging
-from viewModels.viewModel_abstract import viewModelAbstract_T
+from viewModels.viewModel_abstract import viewModel_abstract_T
 
-class ViewAbstract_T:
+class view_abstract_T:
     def __init__(self):
         self.__log = Logger_T()
         self.__log.log(message="Initializing [ViewAbstract_T]", level=logging.INFO)
         self.__viewModel = None
 
-    def addViewModel(self, viewModel : viewModelAbstract_T):
+    def addViewModel(self, viewModel : viewModel_abstract_T):
         if viewModel is None:
             self.__viewModel = viewModel
         else:
